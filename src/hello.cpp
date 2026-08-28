19fd19815721235610db26f2dac189a5149f9d4b
Yash <yashjadeja09@gmail.com>
2026-08-28 15:36:19 -0400

    Coding around
    Just getting to know env

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include<iostream>
 
-int main() {
-    std::cout << "Hello Yashrajsinh System Desinger" << std::endl;
-    return 0;
-  
+int main(){
+  std::cout << ("Yo soi Yash, Je suis Engineer \n");
+  std::cout << ("I am a great Engineer and I study at Waterloo\n");
+  std::cout << ("Last month I turned 26; honestly; not fun as it sounds! \n"  
+  << "27 years is on the way dang!!! \n");
+
 }
