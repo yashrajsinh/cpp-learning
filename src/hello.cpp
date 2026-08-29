5a4fca1dc11ee2877226167c6907cae6728bbce4
Yash <yashjadeja09@gmail.com>
2026-08-29 09:34:56 -0400

    Just minor changes
    Remove some files and wrote custom code

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 
 int main(){
-  std::cout << ("Yo soi Yash, Je suis Engineer \n");
-  std::cout << ("I am a great Engineer and I study at Waterloo\n");
-  std::cout << ("Last month I turned 26; honestly; not fun as it sounds! \n"  
-  << "27 years is on the way dang!!! \n");
-
+ std::cout << "Yo Wsup sheed 26\n" 
+ << 27;
 }
