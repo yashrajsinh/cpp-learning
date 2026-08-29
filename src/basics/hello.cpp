d8c2b32960972b3166479b4d9ec5e845e56f326d
Yash <yashjadeja09@gmail.com>
2026-08-29 10:39:21 -0400

    Playing around with numbers
    learning how to use numbers and cin

--- src/basics/hello.cpp
+++ src/basics/hello.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 
+using namespace std;
+
 int main(){
- std::cout << "Yo Wsup sheed 26\n" 
- << 27;
+    int fav_number = 0;
+    cout << "Yo, why don't you tell me your fav number foo!\n";
+    cin >> fav_number;
+    cout << fav_number << " Cool beans man mine too!\n";
 }
