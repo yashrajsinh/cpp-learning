1b552dd7f54f75bdb292ee779ff44dcdee1eeaa4
Yash <yashjadeja09@gmail.com>
2026-09-01 02:43:08 -0400

    task 1 completed

---
+++ src/basics/favNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int fav_num;
+    cout << "\n-----------------------------------------------------------------\n";
+    cout << "Yo! my man tell me your fav number between 1 and 100:\t";
+    cin >> fav_num;
+    cout << "Cool! That's my fav num too!\nNo really!!," 
+    << fav_num << " is my fav number!";
+     cout << "\n-----------------------------------------------------------------\n";
+   
+}
