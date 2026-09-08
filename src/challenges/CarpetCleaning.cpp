7c7268a6ccc91b00256b8227aee75c7d7c9784ba
mutant Y20 <yashjadeja09@gmail.com>
2026-09-08 08:33:37 -0400

    Section 1 Chllange completed
    Simple math and varible calculator

---
+++ src/challenges/CarpetCleaning.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number_of_small_rooms {0};
+    int number_of_large_rooms {0};
+    const double price_per_small_room {25};
+    const double price_per_large_room {35};
+    double total_cleaning_cost{0};
+    const double tax_percentage{0.06};
+    double tax_amount{0};
+    double total_price_with_tax{0};
+    const int number_of_valid_days{30};
+
+    
+    cout << "\n=============================================\n" 
+    << "Hi, Welcome to Frank's Carpet Cleaning Sevice\n" 
+    << "\nPrice per small room: $" << price_per_small_room
+    << "\nPrice per large room: $" << price_per_large_room
+    << "\nPlease note estimate is valid for " << number_of_valid_days << " days"
+    << "\n=============================================\n"
+    << "\nHow many small rooms would you like to cleaned?\t";
+    cin >> number_of_small_rooms;
+    cout << "How many large rooms would you like to cleaned?\t";
+    cin >> number_of_large_rooms;
+
+    cout << "Estimat for carpet cleaning service\n" 
+       << "Total Small Rooms : " << number_of_small_rooms 
+       << "\nTotal Large Rooms : " << number_of_large_rooms;
+    
+       total_cleaning_cost = {
+        (price_per_large_room * number_of_large_rooms) + 
+        (price_per_small_room * number_of_small_rooms)};
+    cout << "\nTotal cost before tax: $" << total_cleaning_cost;
+   tax_amount = (total_cleaning_cost * tax_percentage);
+   cout << "\nTax: $" << tax_amount 
+   << "\n=============================================\n";
+    total_price_with_tax = (total_cleaning_cost + tax_amount);
+cout << "Total estimate with tax: $" << total_price_with_tax;
+cout << "\n=============================================\n";
+}
