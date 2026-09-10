13062d2dcb04325c478414588ce6e1b2fa7d2e78
mutant Y20 <yashjadeja09@gmail.com>
2026-09-10 06:46:23 -0400

    Updated code

--- src/challenges/CarpetCleaning.cpp
+++ src/challenges/CarpetCleaning.cpp
@@ -1,39 +1,48 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int number_of_small_rooms {0};
-    int number_of_large_rooms {0};
+   
+   
+
+
+
+
+
+
+
     const double price_per_small_room {25};
     const double price_per_large_room {35};
-    double total_cleaning_cost{0};
-    const double tax_percentage{0.06};
-    double tax_amount{0};
-    double total_price_with_tax{0};
     const int number_of_valid_days{30};
-
-    
     cout << "\n=============================================\n" 
     << "Hi, Welcome to Frank's Carpet Cleaning Sevice\n" 
     << "\nPrice per small room: $" << price_per_small_room
     << "\nPrice per large room: $" << price_per_large_room
     << "\nPlease note estimate is valid for " << number_of_valid_days << " days"
-    << "\n=============================================\n"
-    << "\nHow many small rooms would you like to cleaned?\t";
+    << "\n=============================================\n";
+
+    int number_of_small_rooms {0};
+    int number_of_large_rooms {0};
+    cout << "\nHow many small rooms would you like to cleaned?\t";
     cin >> number_of_small_rooms;
     cout << "How many large rooms would you like to cleaned?\t";
     cin >> number_of_large_rooms;
-
     cout << "Estimat for carpet cleaning service\n" 
        << "Total Small Rooms : " << number_of_small_rooms 
        << "\nTotal Large Rooms : " << number_of_large_rooms;
     
+
+    double total_cleaning_cost{0};
        total_cleaning_cost = {
         (price_per_large_room * number_of_large_rooms) + 
         (price_per_small_room * number_of_small_rooms)};
     cout << "\nTotal cost before tax: $" << total_cleaning_cost;
+    double tax_amount{0};
+    const double tax_percentage{0.06};
    tax_amount = (total_cleaning_cost * tax_percentage);
+   
    cout << "\nTax: $" << tax_amount 
    << "\n=============================================\n";
+       double total_price_with_tax{0};
     total_price_with_tax = (total_cleaning_cost + tax_amount);
 cout << "Total estimate with tax: $" << total_price_with_tax;
 cout << "\n=============================================\n";
